dd5d331d24b4dec3ef3751e2eeadc6e137b8c684
Saumyabrat978 <123387394+Saumyabrat978@users.noreply.github.com>
2024-01-10 19:26:01 +0530

    Update middlell.cpp

--- middlell.cpp
+++ middlell.cpp
@@ -23,6 +23,22 @@ public:
     }
 };
 */
+int getmiddle(Node*head){
+    if(head==NULL || head->next==NULL){
+        return head;
+    if(head->next->next==NULL)
+    return head->next;    }
+    Node*slow=head;
+    Node*fast=head->next;
+    while(fast!=NULL){
+        fast=fast->next;
+        if(fast!=NULL){
+            fast=fast->next;
+        }
+        slow=slow->next;
+        return slow;
+    }
+}
 int getlength(Node*head){
     int len=0;
     while(head!=NULL){
@@ -33,8 +49,9 @@ int getlength(Node*head){
     return len;
 }
 Node *findMiddle(Node *head) {
+    return getmiddle(head);
     // Write your code here
-    int len=getlength(head);
+    /*int len=getlength(head);
     int ans=len/2;
     Node*temp=head;
     int cnt=0;
@@ -43,4 +60,4 @@ Node *findMiddle(Node *head) {
         cnt++;
     }
     return temp;
-}
+*/}
