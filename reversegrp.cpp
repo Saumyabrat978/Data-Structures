a52f4c9984d80217eaa7b5b1c4a8a94aa8112c13
Saumyabrat978 <123387394+Saumyabrat978@users.noreply.github.com>
2024-01-11 23:18:33 +0530

    Add files via upload

---
+++ reversegrp.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * class Node {
+ * public:
+ *     int data;
+ *     Node *next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node *next) : data(x), next(next) {}
+ * };
+ */
+Node* kReverse(Node* head, int k) {
+    //base call
+    if(head == NULL) {
+        return NULL;
+    }
+    
+    
+    Node* next = NULL;
+    Node* curr = head;
+    Node* prev = NULL;
+    int count= 0;
+    
+    while( curr != NULL && count < k ) {
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    if (count < k) {
+        curr = prev;
+        prev = NULL;
+        next = NULL;
+        count = 0;
+    }
+        while (curr && count < k)
+        {
+
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+    
+
+    if (next != NULL) {
+        head->next = kReverse(next, k);
+    }
+
+
+
+    return prev;
+}
