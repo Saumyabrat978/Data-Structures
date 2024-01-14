0a02d4431465e8013521428c9cd8b5a64976be32
Saumyabrat <samborkotoky@gail.com>
2024-01-14 09:44:06 +0530

    first commit

---
+++ sort012LL.cpp
@@ -0,0 +1,62 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node* sortList(Node *head){
+    // Write your code here.
+    int cnt0=0;
+    int cnt1=0;
+    int cnt2=0;
+    Node*temp=head;
+    while(temp!=NULL){
+        if(temp->data==0){
+            cnt0++;
+        }
+        else if(temp->data==1){
+            cnt1++;
+        }
+        else if(temp->data==2){
+            cnt2++;
+        }
+        temp=temp->next;
+    }
+    temp=head;
+    while(temp!=NULL){
+        if(cnt0!=0){
+            temp->data=0;
+            cnt0--;
+        }
+        else if(cnt1!=0){
+            temp->data=1;
+            cnt1--;
+        }
+        else if(cnt2!=0){
+            temp->data=2;
+            cnt2--;
+        }
+        temp=temp->next;
+    }
+    return head;
+}
