65af8768787a7fe24df746f3ebc6facd23d9c23f
Saumyabrat <samborkotoky@gail.com>
2024-04-06 09:34:56 +0530

    first commit

---
+++ nonrepeating.cpp
@@ -0,0 +1,26 @@
+class sol{
+    public:
+    string FirstNonR(string A){
+    map<char,int> count;
+    string ans='';
+    queue<char> q;
+    for(int i=0;i<A.length();i++){
+        char ch=A[i];
+        q.push(ch);
+        count[ch]++;
+        while(!q.empty()){
+            if(count[q.front()]>1){
+                q.pop();
+            }
+            else{
+                ans.push_back(q.front());
+                break;
+            }
+        }
+        if(q.empty()){
+            ans.push_back('#');
+        }
+    }
+    }
+    return ans;
+}
